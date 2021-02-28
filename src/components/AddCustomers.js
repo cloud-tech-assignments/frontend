d098b31f19d6695ede0db2dca7fa969a0c81f9ce
Cornelius <sandmal.cornelius@gmail.com>
2021-02-28 23:59:42 +0100

    changed customers and deleted fetch in app

--- src/components/AddCustomers.js
+++ src/components/AddCustomers.js
@@ -3,7 +3,33 @@ import React from 'react';
 function AddCustomers () {
   return (
     <div>
-      Add new customers
+      <form>
+        <label >
+          First name:
+          <input type='text' name='first_name' />
+        </label>
+        <label >
+          Last name:
+          <input type='text' name='second_name' />
+        </label>
+        <label >
+          Date Of Birth:
+          <input type='date' name='date_of_birth' />
+        </label>
+        <label >
+          City:
+          <input type='text' name='city' />
+        </label>
+        <label >
+          Personal Number:
+          <input type='number' name='personal_number' />
+        </label>
+        <label >
+          Account Number:
+          <input type='number' name='account_number' />
+        </label>
+        <input type='submit' value='Update' />
+      </form>
     </div>
   );
 }
--- src/components/UpdateCustomers.js
+++ src/components/UpdateCustomers.js
@@ -6,27 +6,27 @@ function UpdateCustomers () {
       <form>
         <label >
           First name:
-          <input type='text' name='firstName' />
+          <input type='text' name='first_name' />
         </label>
         <label >
           Last name:
-          <input type='text' name='firstName' />
+          <input type='text' name='second_name' />
         </label>
         <label >
           Date Of Birth:
-          <input type='date' name='firstName' />
+          <input type='date' name='date_of_birth' />
         </label>
         <label >
           City:
-          <input type='text' name='firstName' />
+          <input type='text' name='city' />
         </label>
         <label >
           Personal Number:
-          <input type='number' name='firstName' />
+          <input type='number' name='personal_number' />
         </label>
         <label >
           Account Number:
-          <input type='number' name='firstName' />
+          <input type='number' name='account_number' />
         </label>
         <input type='submit' value='Update' />
       </form>
