b7d7f67665ce64ca932aef483b20b29f96c5ce40
Cornelius <sandmal.cornelius@gmail.com>
2021-03-17 03:27:07 +0100

    Todo: add t1-t4 on each page

--- src/components/AddCustomers.js
+++ src/components/AddCustomers.js
@@ -10,10 +10,11 @@ const initialState = {
   account_number: ''
 };
 
-function AddCustomers () {
+function AddCustomers ({onChange}) {
   const [newCustomer, setNewCustomer] = useState(initialState);
+  const [latency, setLatency] = useState('');
 
-  const onChange = (e) => {
+  const onInputChange = (e) => {
     setNewCustomer({
       ...newCustomer,
       [e.target.name]: e.target.value.trim()
@@ -22,42 +23,55 @@ function AddCustomers () {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log({ newCustomer });
-    axios.post('http://localhost:5000/api/customers/', newCustomer).then((response) => {
-      console.log(response);
+    // console.log({ newCustomer });
+    axios.post('https://backend-cloud-01.herokuapp.com/api/customers/', newCustomer, {t2: Date.now()}).then((response) => {
+      console.log(response)
+      const t1 = Date.now();
+      const t2 = response.config.t2
+      const ms = new Date(t1 - t2)
+      setLatency({ms: ms.getMilliseconds()})
+
+      onChange({t1, t2})
+      console.log(" T1: " + t1 + " T2: " + t2 + " Latency: " + Number(t1 - t2))
+      setNewCustomer(
+        response.data.newCustomer
+      )
     });
   };
 
+
   return (
     <div>
+    <p>Latency (T1 - T2): {latency.ms}</p>
+      <h2>Add customer</h2>
       <form onSubmit={onSubmit}>
         <label >
           Personal Number:
-          <input type='number' name='personal_number' onChange={onChange} />
+          <input type='number' name='personal_number' onChange={onInputChange} />
         </label>
         <label >
           First name:
-          <input type='text' name='first_name' onChange={onChange} />
+          <input type='text' name='first_name' onChange={onInputChange} />
         </label>
         <label >
           Last name:
-          <input type='text' name='last_name' onChange={onChange} />
+          <input type='text' name='last_name' onChange={onInputChange} />
         </label>
         <label >
           Date Of Birth:
-          <input type='text' name='date_of_birth' onChange={onChange} />
+          <input type='text' name='date_of_birth' onChange={onInputChange} />
         </label>
         <label >
           City:
-          <input type='text' name='city' onChange={onChange} />
+          <input type='text' name='city' onChange={onInputChange} />
         </label>
-
         <label >
           Account Number:
-          <input type='number' name='account_number' onChange={onChange} />
+          <input type='number' name='account_number' onChange={onInputChange} />
         </label>
         <input type='submit' value='Create' />
       </form>
+
     </div>
   );
 }
--- src/components/UpdateCustomers.js
+++ src/components/UpdateCustomers.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function UpdateCustomers () {
+import './UpdateCustomers.css';
+
+function UpdateCustomers ({onChange}) {
   const [currentCustomer, setCurrentCustomer] = useState({});
   const [customers, setCustomers] = useState([]);
   const [isUpdating, setIsUpdating] = useState(false);
   const [customerPersonalNumber, setCustomerPersonalNumber] = useState(null);
+  const [latency, setLatency] = useState('');
 
   useEffect(() => {
     const fetchCustomers = async () => {
-      const response = await axios.get('http://localhost:5000/api/customers/all');
+      const response = await axios.get('https://backend-cloud-01.herokuapp.com/api/customers/all');
       const newData = await response.data.customers;
       setCustomers(newData);
     };
     fetchCustomers();
   }, []);
 
-  const onChange = (e) => {
+  const onInputChange = (e) => {
     setCurrentCustomer({
       ...currentCustomer,
       [e.target.name]: e.target.value.trim()
@@ -26,9 +29,15 @@ function UpdateCustomers () {
   const onSubmit = (e) => {
     e.preventDefault();
     setIsUpdating(false);
-    axios.put(`http://localhost:5000/api/customers/${ customerPersonalNumber }`, currentCustomer).then((response) => {
+    axios.put(`https://backend-cloud-01.herokuapp.com/api/customers/${ customerPersonalNumber }`, currentCustomer, {t2: Date.now()}).then((response) => {
       const newCustomers = [...customers];
       const { index } = currentCustomer;
+      const t1 = Date.now();
+      const t2 = response.config.t2;
+      const ms = new Date(t1 - t2)
+      setLatency({ms: ms.getMilliseconds()})
+      onChange({t1, t2})
+      console.log(" T1: " + t1 + " T2: " + t2 + " Latency: " + Number(t1 - t2));
       newCustomers[index] = response.data.updatedCustomer;
       setCustomers([...newCustomers]);
     });
@@ -46,50 +55,53 @@ function UpdateCustomers () {
       <form onSubmit={onSubmit}>
         <label>
           Personal Number:
-          <input type='number' name='personal_number' defaultValue={customer.personal_number} onChange={onChange} />
+          <input type='number' name='personal_number' defaultValue={customer.personal_number} onChange={onInputChange} />
         </label>
         <label >
           First name:
-          <input type='text' name='first_name' defaultValue={customer.first_name} onChange={onChange} />
+          <input type='text' name='first_name' defaultValue={customer.first_name} onChange={onInputChange} />
         </label>
         <label >
           Last name:
-          <input type='text' name='last_name' defaultValue={customer.last_name} onChange={onChange} />
+          <input type='text' name='last_name' defaultValue={customer.last_name} onChange={onInputChange} />
         </label>
         <label >
           Date Of Birth:
-          <input type='text' name='date_of_birth' defaultValue={customer.date_of_birth} onChange={onChange} />
+          <input type='text' name='date_of_birth' defaultValue={customer.date_of_birth} onChange={onInputChange} />
         </label>
         <label >
           City:
-          <input type='text' name='city' defaultValue={customer.city} onChange={onChange} />
+          <input type='text' name='city' defaultValue={customer.city} onChange={onInputChange} />
         </label>
         <label >
           Account Number:
-          <input type='number' name='account_number' defaultValue={customer.account_number} onChange={onChange} />
+          <input type='number' name='account_number' defaultValue={customer.account_number} onChange={onInputChange} />
         </label>
-        <input type='submit' value='Update' />
+        <input type='submit' value='Submit' />
       </form>);
   } else {
     return (
       <>
         <div>
+    <p>Latency (T1 - T2): {latency.ms}</p>
           <h2>Select customer to update</h2>
-          {customers.map((customer, i) => (
-            <div key={i}>
-              <h3>
-                {`Personal number: ${ customer.personal_number }`}
-              </h3>
-              <p>First name: {customer.first_name}</p>
-              <p>Last name: {customer.last_name}</p>
-              <p>DOB: {customer.date_of_birth}</p>
-              <p>City: {customer.city}</p>
-              <p>Account number: {customer.account_number}</p>
-              <button onClick={() => {
-                getCustomerIndex(i);
-              }}> Update</button>
-            </div>
-          ))}
+          <div className="customer-container">
+            {customers.map((customer, i) => (
+              <div key={i} className="customer-item">
+                <h3>
+                  {`Personal number: ${ customer.personal_number }`}
+                </h3>
+                <p>First name: {customer.first_name}</p>
+                <p>Last name: {customer.last_name}</p>
+                <p>DOB: {customer.date_of_birth}</p>
+                <p>City: {customer.city}</p>
+                <p>Account number: {customer.account_number}</p>
+                <button className="button" onClick={() => {
+                  getCustomerIndex(i);
+                }}>Update</button>
+              </div>
+            ))}
+          </div>
         </div>
       </>
     );
